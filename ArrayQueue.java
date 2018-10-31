5cb81512f341b5d2f50fa9304bc7053440dee947
atyqahrosslan <41420682+atyqahrosslan@users.noreply.github.com>
2018-10-31 16:32:56 +0800

    Add files via upload

---
+++ ArrayQueue.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ */
+public class ArrayQueue implements QueueInterface{
+    private Object[]a;
+    private int front,back;
+    
+    public ArrayQueue(int capacity){
+        a =new Object[capacity];
+    }
+    
+    public void add(Object object){
+        if (back== a.length) resize();
+     a[back++]= object;
+    }
+    
+    public Object first(){
+        if (size()==0)
+            throw new IllegalStateException("queue is empty");
+        return a[front];
+    }
+    
+    public boolean isEmpty(){
+        return(size()==0);
+    }
+    public Object remove(){
+        if(size() == 0)
+            throw new IllegalStateException("queue is empty");
+        Object object = a[front];
+        a[front++] = null;
+        return object;
+    }
+    
+    public int size(){
+        return back-front;
+    }
+    
+    private void resize(){
+        Object[]aa=a;
+        a=new Object[2*aa.length];
+        System.arraycopy(aa,front,a,0,size());        
+    }
+}//end class
